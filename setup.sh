b98103d72723d89e3ccfa87410defff0e7be0fec
aleksandrdremov <dremov.me@gmail.com>
2021-03-01 11:28:53 +0300

    NASM adapted

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
-# Use this script for versions of Xcode other than Xcode 11.
-
 set -o xtrace
 
 # Create plug-ins directory if it doesn't exist
@@ -11,7 +8,7 @@ if [ ! -d "$plugins_dir" ]; then
 fi
 
 # Copy the IDE Plugin to the plug-ins directory
-cp -r GraphQL.ideplugin $plugins_dir
+cp -r NASM.ideplugin $plugins_dir
 
 # Create Specifications directory if it doesn't exist
 spec_dir=~/Library/Developer/Xcode/Specifications/
@@ -19,6 +16,6 @@ if [ ! -d "$spec_dir" ]; then
 	mkdir $spec_dir
 fi
 
-cp GraphQL.xclangspec $spec_dir
+cp NASM.xclangspec $spec_dir
 
-echo '🎉 Apollo Xcode Add-ons installation has completed! Please restart Xcode and click "Load bundle" when an alert shows about GraphQL.ideplugin.'
+echo '🎉 Xcode Add-ons installation has completed!\nPlease restart Xcode and click "Load bundle" when an alert shows about NASM.ideplugin.'
